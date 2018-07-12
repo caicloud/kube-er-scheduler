9ea56ed622c710107903df61237ed2af524a945c
Wen Gao <gaowen@caicloud.io>
2018-07-12 11:26:43 +0800

    Add test case

--- util_test.go
+++ util_test.go
@@ -25,3 +25,63 @@ func TestUpdateNode(t *testing.T) {
 		t.Fatalf("%v\n", err)
 	}
 }
+
+func TestUpdateNodeER7(t *testing.T) {
+	master := "http://127.0.0.1:8080"
+	var kubeConfig string
+	clientset, err := CreateClientset(&master, &kubeConfig)
+	if err != nil {
+		t.Fatalf("create clientset failed: %v\n", err)
+	}
+	ers := &ExtendedResourceScheduler{
+		Clientset: clientset,
+	}
+	node, err := ers.FindNode("127.0.0.1")
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+	extendedResources := []string{"er7"}
+	node.Status.ExtendedResourceAllocatable = append(node.Status.ExtendedResourceAllocatable, extendedResources...)
+	err = ers.updateNodeStatus(node)
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+}
+
+func TestCleanNodeER(t *testing.T) {
+	master := "http://127.0.0.1:8080"
+	var kubeConfig string
+	clientset, err := CreateClientset(&master, &kubeConfig)
+	if err != nil {
+		t.Fatalf("create clientset failed: %v\n", err)
+	}
+	ers := &ExtendedResourceScheduler{
+		Clientset: clientset,
+	}
+	node, err := ers.FindNode("127.0.0.1")
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+	node.Status.ExtendedResourceAllocatable = []string{}
+	err = ers.updateNodeStatus(node)
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+}
+
+func TestPrintNodeAllocatable(t *testing.T) {
+	master := "http://127.0.0.1:8080"
+	var kubeConfig string
+	clientset, err := CreateClientset(&master, &kubeConfig)
+	if err != nil {
+		t.Fatalf("create clientset failed: %v\n", err)
+	}
+	ers := &ExtendedResourceScheduler{
+		Clientset: clientset,
+	}
+	node, err := ers.FindNode("127.0.0.1")
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+	t.Errorf("extended resource allocatable: %v", node.Status.ExtendedResourceAllocatable)
+}
